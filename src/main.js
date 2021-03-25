a2a9b6383c4a3f19ddd0798095a9586622e7ecce
James Muriithi <muriithijames556@gmail.com>
2021-03-25 23:47:43 +0300

    vuex mutations

--- src/main.js
+++ src/main.js
@@ -1,7 +1,22 @@
 import { createApp } from 'vue';
+import { createStore } from "vuex";
 
 import App from './App.vue';
 
 const app = createApp(App);
+const store = createStore({
+    state(){
+        return {
+            counter: 0
+        }
+    },
+    mutations:{
+        increment(state){
+            state.counter++;
+        }
+    }
+});
+
+app.use(store);
 
 app.mount('#app');
